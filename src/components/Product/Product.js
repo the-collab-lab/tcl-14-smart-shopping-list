49037db48a5351c0b1bf4bcac8b6727c5b226c45
Yesika Castillo <ylcastillo29@hotmail.com>
2020-10-15 13:20:37 -0500

    capture form data

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,31 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 function Product() {
+  const [name, setName] = useState('');
+  const [selectedOption, setSelectedOption] = useState();
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    console.log(name, selectedOption);
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={submitForm}>
         <div>
           <label>
             Name:
-            <input type="text" name="name" />
+            <input
+              type="text"
+              name="name"
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+            />
           </label>
         </div>
         <div>
-          <input type="checkbox" name="Soon" />
-          <label> Soon (in the next 7 days) </label>
-        </div>
-
-        <div>
-          <input type="checkbox" name="kindSoon" />
-          <label> Kind of soon (in the next 14 days) </label>
-        </div>
-        <div>
-          <input type="checkbox" name="notSoon" />
-          <label> Not soon (in the next 30 days) </label>
+          <h4>How soon will you buy</h4>
+          <div>
+            <input
+              type="radio"
+              name="date"
+              id="soon"
+              value="Soon (in the next 7 days)"
+              onClick={() => setSelectedOption('Soon (in the next 7 days)')}
+            />
+            <label> Soon (in the next 7 days)</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="date"
+              id="kind"
+              value=" Kind of soon (in the next 14 days)"
+              onClick={() =>
+                setSelectedOption('Kind of soon (in the next 14 days)')
+              }
+            />
+            <label> Kind of soon (in the next 14 days) </label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="date"
+              id="notSoon"
+              value="Not soon (in the next 30 days)"
+              onClick={() =>
+                setSelectedOption('Not soon (in the next 30 days)')
+              }
+            />
+            <label> Not soon (in the next 30 days) </label>
+          </div>
         </div>
         <div>
           <label>
-            Last purchased date
+            <h4>Last purchased date</h4>
             <input type="text" name="date" />
           </label>
         </div>
