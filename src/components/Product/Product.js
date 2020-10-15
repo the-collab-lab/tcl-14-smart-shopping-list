9fb76125e00c02450b03dfd05d9d5b41fb4416e8
arche4 <manuela_jimenez23141@elpoli.edu.co>
2020-10-15 08:25:33 -0500

    The from is created for add product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
 
 function Product() {
-  return <div>hola prueba</div>;
+  return (
+    <div>
+      <form>
+        <div>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <input type="checkbox" name="Soon" />
+          <label> Soon (in the next 7 days) </label>
+        </div>
+
+        <div>
+          <input type="checkbox" name="kindSoon" />
+          <label> Kind of soon (in the next 14 days) </label>
+        </div>
+        <div>
+          <input type="checkbox" name="notSoon" />
+          <label> Not soon (in the next 30 days) </label>
+        </div>
+        <div>
+          <label>
+            Last purchased date
+            <input type="text" name="date" />
+          </label>
+        </div>
+        <div>
+          <input type="submit" value="Save" name="Save" />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Product;
