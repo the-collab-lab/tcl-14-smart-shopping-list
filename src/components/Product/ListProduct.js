ee3b3bb4989942a9c214816722a4a3bf296a96a2
unknown <lizethb.orozco@gmail.com>
2020-10-31 07:39:24 -0500

    remove {} in the line 37

--- src/components/Product/ListProduct.js
+++ src/components/Product/ListProduct.js
@@ -35,7 +35,7 @@ export default function ListProduct() {
                 ))}
               </tbody>
             </table>
-            {<Product />}
+            <Product />
           </>
         );
       }}
