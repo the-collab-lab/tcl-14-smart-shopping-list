746ee7d8eaecb7909b56dd0297f460157990ab84
arche4 <manuela_jimenez23141@elpoli.edu.co>
2020-11-08 12:53:49 -0500

    the differenceInDays library is removed

--- src/components/Product/ListProduct.js
+++ src/components/Product/ListProduct.js
@@ -14,6 +14,7 @@ export default function ListProduct() {
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
     return diffDays;
   };
+
   const select = (product) => {
     const numberOfPurchases = product.numberPurchases + 1;
     const lastPurchase = new Date();
